8fa8d266498f5f1ae5e4512dabf4339d5219df20
CarolineGomes9 <106562690+CarolineGomes9@users.noreply.github.com>
2025-01-31 13:45:36 -0300

    Update leilao.sql

--- leilao.sql
+++ leilao.sql
@@ -7,7 +7,7 @@ CREATE TABLE Leilao (
     DataHoraInicio TIMESTAMP NOT NULL,
     DataHoraFim TIMESTAMP NOT NULL,
     PrecoInicial DECIMAL(10,2) NOT NULL CHECK (precoinicial >= 0),
-    IdUsuarioCriador INTEGER NOT NULL,
+    idUsuarioCriador INTEGER NOT NULL,
     idLanceGanhador INTEGER NULL,
     FOREIGN KEY (idUsuarioCriador) REFERENCES Usuario (idUsuario) ON DELETE SET NULL
 );
