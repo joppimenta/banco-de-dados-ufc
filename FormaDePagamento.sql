5a68d1e3039a8b7bb096c6c94c0ebe98569dc52f
CarolineGomes9 <106562690+CarolineGomes9@users.noreply.github.com>
2025-01-31 13:50:30 -0300

    Create FormaDePagamento.sql

---
+++ FormaDePagamento.sql
@@ -0,0 +1,8 @@
+
+CREATE TABLE FormaDePagamento(
+idLeilao INTEGER NOT NULL,
+NomeFormaDePagamento VARCHAR(20) NOT NULL,
+PRIMARY KEY(idLeilao, NomeFormaDePagamento),
+FOREIGN KEY(idLeilao) REFERENCES Leilao (idLeilao)
+);
+
