b74cc1be17ae8ae413bbe7bb1528bb12252455d6
CarolineGomes9 <106562690+CarolineGomes9@users.noreply.github.com>
2025-01-31 13:58:09 -0300

    Update FormaDePagamento.sql

--- FormaDePagamento.sql
+++ FormaDePagamento.sql
@@ -3,6 +3,6 @@ CREATE TABLE FormaDePagamento(
 idLeilao INTEGER NOT NULL,
 NomeFormaDePagamento VARCHAR(20) NOT NULL,
 PRIMARY KEY(idLeilao, NomeFormaDePagamento),
-FOREIGN KEY(idLeilao) REFERENCES Leilao (idLeilao)
+FOREIGN KEY(idLeilao) REFERENCES Leilao (idLeilao) ON DELETE CASCADE
 );
 
