e6b55caf432997fc4018e185cb8c8318d9437512
João Pedro Pimenta <joaopedropimenta@alu.ufc.br>
2025-02-23 10:52:54 -0300

    etapa 7 users add

---
+++ Etapa 7/usuarios.sql
@@ -0,0 +1,21 @@
+CREATE ROLE administrador
+
+Grant select, insert, delete on avaliacao, categoria, formadepagamento,
+lance, leilao, possuicategoria,usuario
+to administrador 
+with grant option
+
+CREATE USER joao_pimenta WITH PASSWORD 'joao123'
+
+GRANT administrador TO joao_pimenta
+
+CREATE ROLE usuario_convencional
+
+Grant select on avaliacao, categoria, formadepagamento,
+lance, leilao, possuicategoria,usuario
+to usuario_convencional
+with grant option
+
+CREATE USER fulano WITH PASSWORD 'fulanin'
+
+GRANT usuario_convencional TO fulano
